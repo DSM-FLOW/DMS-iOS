ce324cf031b72c17f18aece7e1160440ddcca1c4
ray3238 <jjmh209@gmail.com>
2024-04-25 15:30:13 +0900

    feat :: OutingAPI 의존성 추가

--- Projects/Feature/OutingApplyFeature/Sources/OutingApplyComponent.swift
+++ Projects/Feature/OutingApplyFeature/Sources/OutingApplyComponent.swift
@@ -1,17 +1,33 @@
 import SwiftUI
+import StudentsDomainInterface
 import OutingDomainInterface
 import NeedleFoundation
 import OutingApplyFeatureInterface
 
 public protocol OutingApplyDependency: Dependency {
     var fetchMyOutingApplicationItemUseCase: any FetchMyOutingApplicationItemUseCase { get }
+    var fetchOutingAvailableTimeUseCase: any FetchOutingAvailableTimeUseCase { get }
+    var fetchOutingTypeUseCase: any FetchOutingTypeUseCase { get }
+    var deleteOutingApplicationItemUseCase: any DeleteOutingApplicationItemUseCase { get }
+    var fetchAllStudentUseCase: any FetchAllStudentUseCase { get }
+    var outingApplicationUseCase: any OutingApplicationUseCase { get }
 }
 
 public final class OutingApplyComponent: Component<OutingApplyDependency>, OutingApplyFactory {
     public func makeView() -> some View {
         OutingApplyView(
             viewModel: .init(
-                fetchMyOutingApplicationItemUseCase: dependency.fetchMyOutingApplicationItemUseCase
+                fetchOutingAvailableTimeUseCase: dependency.fetchOutingAvailableTimeUseCase,
+
+                fetchMyOutingApplicationItemUseCase: dependency.fetchMyOutingApplicationItemUseCase,
+
+                fetchOutingTypeUseCase: dependency.fetchOutingTypeUseCase,
+
+                deleteOutingApplicationItemUseCase: dependency.deleteOutingApplicationItemUseCase,
+
+                fetchAllStudentUseCase: dependency.fetchAllStudentUseCase,
+
+                outingApplicationUseCase: dependency.outingApplicationUseCase
             )
         )
     }
