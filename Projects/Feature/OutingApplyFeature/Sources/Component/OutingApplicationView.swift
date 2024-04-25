de6e864602318e92190ee737c97735f83a00b08c
ray3238 <jjmh209@gmail.com>
2024-04-25 15:23:11 +0900

    feat :: outingTextField에 필수 입력 사항 status 추가

--- Projects/Feature/OutingApplyFeature/Sources/Component/OutingApplicationView.swift
+++ Projects/Feature/OutingApplyFeature/Sources/Component/OutingApplicationView.swift
@@ -2,27 +2,35 @@ import DesignSystem
 import SwiftUI
 
 struct OutingApplyTextField: View {
-    var title: String
+    @State var title: String
     var placeholder: String
     @Binding var text: String
+    var status: Bool?
 
     public init(
         _ title: String = "",
         _ placeholder: String = "",
-        _ text: Binding<String>
+        _ text: Binding<String>,
+        _ status: Bool? = false
     ) {
         self.title = title
         self.placeholder = placeholder
         _text = text
+        self.status = status
     }
 
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title)
-                .dmsFont(.body(.body2), color: .GrayScale.gray10)
-                .padding(.leading, 1)
-                .padding(.bottom, 8)
-
+            HStack {
+                Text(title)
+                    .dmsFont(.body(.body2), color: .GrayScale.gray10)
+                    .padding(.leading, 1)
+                    .padding(.bottom, 8)
+                if self.status ?? false {
+                    Text("*")
+                        .dmsFont(.body(.body3), color: .PrimaryVariant.primary)
+                }
+            }
             DMSFormTextField(placeholder, text: $text)
         }
     }
