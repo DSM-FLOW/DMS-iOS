a44e52d5359efd282ae14448ca4e8470f6f9c322
ray3238 <jjmh209@gmail.com>
2024-04-25 15:28:02 +0900

    feat :: RecentOutingApplyView 디자인 수정

--- Projects/Feature/OutingApplyFeature/Sources/Component/RecentOutingApplyView.swift
+++ Projects/Feature/OutingApplyFeature/Sources/Component/RecentOutingApplyView.swift
@@ -2,13 +2,13 @@ import DesignSystem
 import SwiftUI
 
 struct RecentOutingApplyView: View {
+    @StateObject var viewModel: OutingApplyViewModel
     let date: String
     let type: String
     let isAccepted: Bool
     let startTime: String
     let endTime: String
     let people: String
-    let place: String
     let reason: String
 
     var body: some View {
@@ -19,52 +19,55 @@ struct RecentOutingApplyView: View {
                 .padding(.bottom, 10)
 
             ZStack(alignment: .topTrailing) {
-                VStack(alignment: .leading) {
-                    Text(date)
-                        .dmsFont(.body(.body3), color: .PrimaryVariant.primary)
-                        .padding(.top, 3)
-                        .padding(.bottom, 8)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(date)
+                            .dmsFont(.body(.body3), color: .PrimaryVariant.primary)
+                            .padding(.top, 3)
 
-                    Text(type)
-                        .dmsFont(.title(.title3), color: .GrayScale.gray10)
+                        Text(type)
+                            .dmsFont(.title(.title3), color: .GrayScale.gray10)
 
-                    Text("\(startTime) ~ \(endTime)")
+                        Text("\(startTime) ~ \(endTime)")
+                            .dmsFont(.body(.body2), color: .GrayScale.gray6)
+                            .padding(.bottom, 16)
+
+                        HStack {
+                            Text("동행자 : ")
+                                .dmsFont(.etc(.button), color: .GrayScale.gray7)
+
+                            Text(people)
+                                .dmsFont(.body(.body3), color: .GrayScale.gray7)
+                        }
+
+                        Text(reason)
+                            .dmsFont(.body(.body3), color: .GrayScale.gray6)
+                            .padding(.horizontal, 12)
+                            .padding(.vertical, 16)
+                            .background(Color.GrayScale.gray2)
+                            .cornerRadius(10)
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        DMSButton(text: "외출 취소", style: .underline, color: .red) {
+                            viewModel.deleteOutingApplicationItemButtonDidClicked()
+                        }
+                        Spacer()
+                    }
+                }
+                .alert("", isPresented: $viewModel.isPresentedDeleteOutingApplicationItemAlert) {
+                    Button("취소", role: .cancel) {}
+                    Button("확인",
+                           role: .destructive,
+                           action: viewModel.confirmDeleteOutingApplicationItemButtonDidClicked
+                    )
+                } message: {
+                    Text("외출 신청을 취소하시겠습니까?")
                         .dmsFont(.body(.body2), color: .GrayScale.gray6)
-                        .padding(.bottom, 26)
-
-                    HStack {
-                        Text("동행자 : ")
-                            .dmsFont(.etc(.button), color: .GrayScale.gray7)
-
-                        Text(people)
-                            .dmsFont(.body(.body3), color: .GrayScale.gray7)
-                    }
-
-                    HStack {
-                        Text("외출 장소 : ")
-                            .dmsFont(.etc(.button), color: .GrayScale.gray7)
-
-                        Text(place)
-                            .dmsFont(.body(.body3), color: .GrayScale.gray7)
-                    }
-
-                    Text(reason)
-                        .dmsFont(.body(.body3), color: .GrayScale.gray6)
-                        .padding(.horizontal, 12)
-                        .padding(.vertical, 16)
-                        .background(Color.GrayScale.gray2)
-                        .cornerRadius(10)
                 }
-
-                if isAccepted {
-                    Text("수락됨")
-                        .dmsFont(.etc(.button), color: .PrimaryVariant.primary)
-                        .padding(.vertical, 8)
-                        .padding(.horizontal, 16)
-                        .background(Color.PrimaryVariant.lighten2)
-                        .cornerRadius(24)
-                }
+                .dmsToast(isShowing: $viewModel.isErrorOcuured, message: viewModel.errorMessage, style: .error)
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             .padding(20)
             .background(Color.System.surface)
             .cornerRadius(10)
