a29d0cc865983ac2c4ee53bee5ee6b6218eb6bac
ray3238 <jjmh209@gmail.com>
2024-04-26 17:29:28 +0900

    refactor :: timePickerView 리펙

--- Projects/Feature/OutingApplyFeature/Sources/Component/OutingTimePickerView.swift
+++ Projects/Feature/OutingApplyFeature/Sources/Component/OutingTimePickerView.swift
@@ -4,10 +4,7 @@ import SwiftUI
 struct OutingTimePickerView: View {
     @StateObject var viewModel: OutingApplyViewModel
     @Binding var timeText: String
-    @State var outingHourTimeList = Array(1...24)
-    @State var outingMinuteTimeList = Array(0...59)
-    @State var arrivalHourTimeList = Array(1...24)
-    @State var arrivalMinuteTimeList = Array(0...59)
+    @State var selectedTime = SelectedTime()
     @State var outingHourSelectTime: Int = 0
     @State var outingMinuteSelectTime: Int = 0
     @State var arrivalHourSelectTime: Int = 0
@@ -15,51 +12,93 @@ struct OutingTimePickerView: View {
     @State var isPressed = false
 
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack() {
-                Picker("OutingHourTime", selection: $outingHourSelectTime) {
-                    ForEach(0 ..< outingHourTimeList.count) {
-                        Text("\(outingHourTimeList[$0])")
-                            .padding(.horizontal, 10)
+        VStack {
+            Spacer()
+
+            ZStack(alignment: .center) {
+                RoundedRectangle(cornerRadius: 4)
+                    .foregroundColor(Color.PrimaryVariant.lighten1.opacity(0.5))
+                    .frame(height: 42)
+
+                HStack(alignment: .center) {
+                    Spacer()
+
+                    Picker("OutingHourTime", selection: $selectedTime.outingHour) {
+                        ForEach(1 ..< 25) { num in
+                            Text("\(num)")
+                                .dmsFont(.body(.body1), color: .GrayScale.gray10)
+                                .padding(.horizontal, 8)
+                        }
                     }
-                }
-                .pickerStyle(.inline)
-                Text(" : ")
-                Picker("OutingMinuteTime", selection: $outingMinuteSelectTime) {
-                    ForEach(0 ..< outingMinuteTimeList.count) {
-                        Text("\(outingMinuteTimeList[$0])")
-                            .padding(.horizontal, 10)
+                    .pickerStyle(.inline)
+                    .listRowBackground(Color.clear)
+
+                    Text(" : ")
+
+                    Picker("OutingMinuteTime", selection: $selectedTime.outingMinute) {
+                        ForEach(0 ..< 60) { num in
+                            Text("\(num)")
+                                .dmsFont(.body(.body1), color: .GrayScale.gray10)
+                                .padding(.horizontal, 8)
+                        }
                     }
-                }
-                .pickerStyle(.inline)
-                Text(" ~ ")
-                Picker("arrivalHourTime", selection: $arrivalHourSelectTime) {
-                    ForEach(0 ..< arrivalHourTimeList.count) {
-                        Text("\(arrivalHourTimeList[$0])")
-                            .padding(.horizontal, 10)
+                    .pickerStyle(.inline)
+                    .listRowBackground(Color.clear)
+
+                    Spacer()
+
+                    Text(" ~ ")
+                        .dmsFont(.title(.title1), color: .GrayScale.gray10)
+
+                    Spacer()
+
+                    Picker("arrivalHourTime", selection: $selectedTime.arrivalHour) {
+                        ForEach(1 ..< 25) { num in
+                            Text("\(num)")
+                                .dmsFont(.body(.body1), color: .GrayScale.gray10)
+                                .padding(.horizontal, 8)
+                        }
                     }
-                }
-                .pickerStyle(.inline)
-                Text(" : ")
-                Picker("OutingMinuteTime", selection: $arrivalMinuteSelectTime) {
-                    ForEach(0 ..< arrivalMinuteTimeList.count) {
-                        Text("\(arrivalMinuteTimeList[$0])")
-                            .padding(.horizontal, 10)
+                    .listRowBackground(Color.clear)
+                    .pickerStyle(.inline)
+
+                    Text(" : ")
+
+                    Picker("OutingMinuteTime", selection: $selectedTime.arrivalMinute) {
+                        ForEach(0 ..< 60) { num in
+                            Text("\(num)")
+                                .dmsFont(.body(.body1), color: .GrayScale.gray10)
+                                .padding(.horizontal, 8)
+                        }
                     }
+                    .listRowBackground(Color.clear)
+                    .pickerStyle(.inline)
+
+                    Spacer()
                 }
-                .pickerStyle(.inline)
+                .padding(.vertical, 7)
+                .cornerRadius(4)
             }
-            .padding(.horizontal, 6)
+
             DMSWideButton(text: "확인", color: .PrimaryVariant.primary) {
-                viewModel.outingHourSelectTime = outingHourSelectTime + 1
-                viewModel.outingMinuteSelectTime = outingMinuteSelectTime
-                viewModel.arrivalHourSelectTime = arrivalHourSelectTime + 1
-                viewModel.arrivalMinuteSelectTime = arrivalMinuteSelectTime
+                viewModel.outingHourSelectTime = selectedTime.outingHour + 1
+                viewModel.outingMinuteSelectTime = selectedTime.outingMinute
+                viewModel.arrivalHourSelectTime = selectedTime.arrivalHour + 1
+                viewModel.arrivalMinuteSelectTime = selectedTime.arrivalMinute
                 timeText = viewModel.outingApplicationTime
                 viewModel.outingApplicationTimeStatus = true
+                viewModel.isShowingOutingTimePickerBottomSheet = false
             }
+            .dmsFont(.etc(.button))
             .padding(.top, 30)
-            .padding(.horizontal, 24)
         }
+        .padding(.horizontal, 24)
     }
+}
+
+struct SelectedTime {
+    var outingHour = 0
+    var outingMinute = 0
+    var arrivalHour = 0
+    var arrivalMinute = 0
 }
