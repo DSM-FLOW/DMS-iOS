c0acee6dd3e235895441a457691ad8629b293627
ray3238 <jjmh209@gmail.com>
2024-05-10 19:41:45 +0900

    fix :: 상벌점 dateFormat

--- Projects/Domain/PointsDomain/Interface/DTO/DataTransfer/FetchPointListTransfer.swift
+++ Projects/Domain/PointsDomain/Interface/DTO/DataTransfer/FetchPointListTransfer.swift
@@ -1,15 +1,12 @@
 import Combine
+import UtilityModule
 import Foundation
 
 public extension FetchPointListResponseDTO.SinglePointResponseDTO {
     func toDomain() -> PointEntity.SinglePoint {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-        formatter.locale = Locale(identifier: "ko_kr")
-        let date: Date = formatter.date(from: date) ?? .init()
-        return PointEntity.SinglePoint(
+        PointEntity.SinglePoint(
             pointID: pointID,
-            date: date,
+            date: date.toSmallDMSDate(),
             type: PointsType(rawValue: type) ?? .bonus,
             name: name,
             score: score
