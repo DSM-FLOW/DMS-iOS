9bbc80e8a85a6bf0df382c5b2ec3a37eb857fa5c
ray3238 <jjmh209@gmail.com>
2024-05-10 20:01:53 +0900

    refactor :: 옵셔널 처리

--- Projects/Feature/OutingApplyFeature/Sources/Component/OutingTextFieldView.swift
+++ Projects/Feature/OutingApplyFeature/Sources/Component/OutingTextFieldView.swift
@@ -27,7 +27,7 @@ struct OutingTextFieldView: View {
                     .padding(.leading, 1)
                     .padding(.bottom, 8)
 
-                if isRequired {
+                if (isRequired ?? false) {
                     Text("*")
                         .dmsFont(.body(.body3), color: .PrimaryVariant.primary)
                 }
