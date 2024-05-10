2a6336ec8a69e7bd29e65a68b3f402dc2f2620a8
ray3238 <jjmh209@gmail.com>
2024-05-10 19:33:22 +0900

    perf :: 에러 메세지 수정

--- Projects/Domain/OutingDomain/Interface/Error/OutingDomainError.swift
+++ Projects/Domain/OutingDomain/Interface/Error/OutingDomainError.swift
@@ -19,7 +19,7 @@ extension OutingDomainError: LocalizedError {
             return "인증이 만료되었습니다. 다시 로그인해주세요."
 
         case .forbidden:
-            return "올바른 시간인지 확인해주세요."
+            return "외출 신청 시간을 확인해주세요."
 
         case .tooManyRequest:
             return "요청 횟수를 초과했습니다. 잠시 후 다시 시도해주세요."
@@ -28,7 +28,7 @@ extension OutingDomainError: LocalizedError {
             return "서버에 문제가 발생하였습니다. 잠시 후 다시 시도해주세요."
 
         case .notFound:
-            return "신청된 외출이 없습니다."
+            return "외출 신청이 없습니다."
         }
     }
 }
