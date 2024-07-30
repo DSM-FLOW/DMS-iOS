a1f014ebb355a1774d1d6fadb0eece8a9f249d26
ray3238 <jjmh209@gmail.com>
2024-07-31 08:20:14 +0900

    feat :: 토글 이벤트 적용

--- Projects/Feature/MyPageFeature/Sources/SetNotification/SetNotificationComponent.swift
+++ Projects/Feature/MyPageFeature/Sources/SetNotification/SetNotificationComponent.swift
@@ -5,13 +5,15 @@ import NotificationDomainInterface
 
 public protocol SetNotificationDependency: Dependency {
     var subscribeTopicUseCase: any SubscribeTopicUseCase { get }
+    var unsubscribeTopicUseCase: any UnsubscribeTopicUseCase { get }
 }
 
 public final class SetNotificationComponent: Component<SetNotificationDependency>, SetNotificationFactory {
     public func makeView() -> some View {
         SetNotificationView(
             viewModel: .init(
-                subscribeTopicUseCase: self.dependency.subscribeTopicUseCase
+                subscribeTopicUseCase: self.dependency.subscribeTopicUseCase,
+                unsubscribeTopicUseCase: self.dependency.unsubscribeTopicUseCase
             )
         )
     }
--- Projects/Feature/MyPageFeature/Sources/SetNotification/SetNotificationViewModel.swift
+++ Projects/Feature/MyPageFeature/Sources/SetNotification/SetNotificationViewModel.swift
@@ -4,15 +4,100 @@ import Combine
 
 final class SetNotificationViewModel: BaseViewModel {
     private let subscribeTopicUseCase: any SubscribeTopicUseCase
-    @Published var isNoticeOn = false
-    @Published var isTimeSlotOn = false
-    @Published var isAvailableTimeOn = false
-    @Published var isRewardOn = false
-    @Published var isOutingOn = false
+    private let unsubscribeTopicUseCase: any UnsubscribeTopicUseCase
+    @Published var isNoticeOn = false {
+        didSet {
+            noticeToggleButtonDidClicked()
+        }
+    }
+    @Published var isTimeSlotOn = false {
+        didSet {
+            timeSlotToggleButtonDidClicked()
+        }
+    }
+    @Published var isAvailableTimeOn = false {
+        didSet {
+            availableTimeToggleButtonDidClicked()
+        }
+    }
+    @Published var isRewardOn = false {
+        didSet {
+            rewardToggleButtonDidClicked()
+        }
+    }
+    @Published var isOutingOn = false {
+        didSet {
+            outingToggleButtonDidClicked()
+        }
+    }
 
     public init(
-        subscribeTopicUseCase: any SubscribeTopicUseCase
+        subscribeTopicUseCase: any SubscribeTopicUseCase,
+        unsubscribeTopicUseCase: any UnsubscribeTopicUseCase
     ) {
         self.subscribeTopicUseCase = subscribeTopicUseCase
+        self.unsubscribeTopicUseCase = unsubscribeTopicUseCase
+    }
+
+    func noticeToggleButtonDidClicked() {
+        if isNoticeOn {
+            print("noticeToggleButtonDidClicked~!")
+            print("true")
+//            addCancellable(
+//                subscribeTopicUseCase.execute(
+//                    token: "",
+//                    topic: .notice
+//                )
+//            )
+        } else {
+            print("noticeToggleButtonDidClicked~!")
+            print("false")
+//            addCancellable(
+//                unsubscribeTopicUseCase.execute(
+//                    token: "",
+//                    topic: .notice
+//                )
+//            )
+        }
+    }
+
+    func timeSlotToggleButtonDidClicked() {
+        if isTimeSlotOn {
+            print("timeSlotToggleButtonDidClicked~!")
+            print("true")
+        } else {
+            print("timeSlotToggleButtonDidClicked~!")
+            print("false")
+        }
+    }
+
+    func availableTimeToggleButtonDidClicked() {
+        if isAvailableTimeOn {
+            print("availableTimeToggleButtonDidClicked~!")
+            print("true")
+        } else {
+            print("availableTimeToggleButtonDidClicked~!")
+            print("false")
+        }
+    }
+
+    func rewardToggleButtonDidClicked() {
+        if isRewardOn {
+            print("rewardToggleButtonDidClicked~!")
+            print("true")
+        } else {
+            print("rewardToggleButtonDidClicked~!")
+            print("false")
+        }
+    }
+
+    func outingToggleButtonDidClicked() {
+        if isOutingOn {
+            print("outingToggleButtonDidClicked~!")
+            print("true")
+        } else {
+            print("outingToggleButtonDidClicked~!")
+            print("false")
+        }
     }
 }
