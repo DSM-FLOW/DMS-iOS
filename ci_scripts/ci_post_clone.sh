0da2d2daa5bee284b1d7c6a07e6a0afa8a08dd48
InhyeKang <kanginhae0826@gmail.com>
2024-08-14 09:17:06 +0900

    ci :: ci_install_tuist 삭제 및 ci_post_clone mise 설치

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,5 +4,25 @@ cd ../
 git clone https://github.com/team-aliens/DMS-XCConfig.git
 mv DMS-XCConfig/XCConfig/ .
 
-chmod +x ci_install_tuist.sh
-./ci_install_tuist.sh
+set -e
+cd ..
+
+curl https://mise.run | sh
+export PATH="$HOME/.local/bin:$PATH"
+
+# Output the current PATH for debugging
+echo "Current PATH: $PATH"
+
+echo "mise version"
+mise --version
+echo "mise install"
+mise install # Installs the version from .mise.toml
+eval "$(mise activate bash --shims)"
+
+echo "mise doctor"
+mise doctor
+echo "tuist install"
+tuist install
+echo "tuist generate"
+TUIST_CI=1
+tuist generate
